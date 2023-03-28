bc1fb43013ce1e119196fda4e2ffc5c3fe9aa701
Sowmya Naik <naik_sowmya@lilly.com>
2023-03-28 09:48:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import { Routes, Route } from 'react-router-dom';
-import About from './pages/About';
-import Homepage from './pages/Homepage';
+import logo from './logo.svg';
+import './App.css';
+import { usePapaParse } from 'react-papaparse';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const { readRemoteFile } = usePapaParse();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    readRemoteFile('/react-gh-website/data/data_file.csv', {
+      header: true,
+      complete: (results) => {
+        setData(results.data);
+      },
+    });
+  }, [readRemoteFile]);
+
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+        <main>
+          <ul>
+            {data.map((el, i) => (
+              <li key={i}>
+                {el.id} - {el.Name} - {el.value}
+              </li>
+            ))}
+          </ul>
+        </main>
+      </div>
     </>
   );
 };
