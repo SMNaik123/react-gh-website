80bc8726d640ddb8339b6241dcf9e22b44828b5f
Sowmya Naik <naik_sowmya@lilly.com>
2023-03-28 17:02:03 +0530

    update fetch api

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import { usePapaParse } from 'react-papaparse';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 const App = () => {
   const { readString } = usePapaParse();
   const [data, setData] = useState([]);
   const [lastModified, setLastModified] = useState('0');
 
+  const fetchFile = useCallback(async () => {
+    const res = await fetch('/react-gh-website/data/data_file.csv');
+    const csvRestoJson = readString(await res.text(), {
+      header: true,
+    }).data;
+    const lm = res.headers.get('last-modified');
+    if (new Date(lm) > new Date(lastModified)) {
+      setLastModified(lm);
+      setData(csvRestoJson);
+    }
+  }, [readString, lastModified]);
+
   useEffect(() => {
-    const fetchFile = async () => {
-      const res = await fetch('/react-gh-website/data/data_file.csv');
-      const csvRestoJson = readString(await res.text(), {
-        header: true,
-      }).data;
-      const lm = res.headers.get('last-modified');
-      if (lastModified.length > 0 && new Date(lm) > new Date(lastModified)) {
-        setLastModified(lm);
-        setData(csvRestoJson);
-      }
-    };
     fetchFile();
     // run every 5 minuted
     const fetchAPITimer = setInterval(() => {
       fetchFile();
-    }, 1000 * 60 * 5);
+    }, 1000 * 30);
 
     return () => {
       clearInterval(fetchAPITimer);
     };
-  }, [readString]);
+  }, [fetchFile]);
 
   return (
     <>
